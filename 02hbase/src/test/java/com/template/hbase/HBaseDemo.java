d21203a961d3b495a55de15be7069c28ddc8a038
sb3h <470441372@qq.com>
2017-06-13 10:28:41 +0800

    更名为大数据相关demo，非业务demo

---
+++ 02hbase/src/test/java/com/template/hbase/HBaseDemo.java
@@ -0,0 +1,28 @@
+package com.template.hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by huanghh on 2017/6/13.
+ */
+public class HBaseDemo {
+    @Before
+    public void init() throws IOException {
+
+    }
+
+    @Test
+    public void testRun() throws IOException {
+
+    }
+
+    @After
+    public void end() throws IOException {
+    }
+}
